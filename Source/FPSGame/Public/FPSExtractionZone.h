6504c5aef4ec4c9db4c289890e5b7bd6735d7b96
Adam <Twilightsparkleadam@gmail.com>
2018-06-10 02:50:44 +0300

    Added MissingObjective sound and gameover widget

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -21,11 +21,12 @@ protected:
 	class UBoxComponent* OverlapComp;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-	class UDecalComponent* DecalComp;
+	UDecalComponent* DecalComp;
 
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
 		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
-	
+	UPROPERTY(EditDefaultsOnly, Category = "Sounds")
+	USoundBase* ObjectiveMissingSound;
 };

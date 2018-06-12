c3947bafb37deb3476d6df6b04a1938a92865810
Adam <Twilightsparkleadam@gmail.com>
2018-06-12 11:19:38 +0300

    fixed bool

--- Source/FPSGame/Private/FPSGameState.cpp
+++ Source/FPSGame/Private/FPSGameState.cpp
@@ -1,9 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSGameState.h"
-#include "Engine/World.h"
 
-void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSucess)
+void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSuccess)
 {
 	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++) {
 		APawn* Pawn = It->Get();

f9dcc0c3764a491257f9d7c9228ebf89efc63fdb
Adam <Twilightsparkleadam@gmail.com>
2018-06-12 16:19:32 +0300

    Revert "fixed bool"
    This reverts commit c3947bafb37deb3476d6df6b04a1938a92865810.

--- Source/FPSGame/Private/FPSGameState.cpp
+++ Source/FPSGame/Private/FPSGameState.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSGameState.h"
+#include "Engine/World.h"
 
-void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSuccess)
+void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSucess)
 {
 	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++) {
 		APawn* Pawn = It->Get();
